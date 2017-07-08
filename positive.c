d4dff22a7b0113e28d989b3da20c0804d3b5785d
dgiannelli <iannelli.giovanni@gmail.com>
2017-07-08 16:43:27 +0200

    finished positive

--- positive.c
+++ positive.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <gsl/gsl_math.h>
 
 #include "functions.h"
 
-#define C 41.47 // hbar^2/m MeV*fm^2
+#define K 41.47 // hbar^2/m MeV*fm^2
 
-#define R 40. //fm -> Tail point 
-#define N 10000 // Number of points in the left part
-#define H (R/(N-1)) //fm -> step
+#define R 40. // Tail point (in fm)
+#define N 40000 // Number intervals
+#define H (R/N) // Step of the discretization (in fm)
 
 double E;
 
@@ -17,26 +18,21 @@ double *X, *Y;
 // Function for Numerov algorithm
 double W(double r)
 {
-    return (Gauss(r) - E)/C;
+    return (Gauss(r) - E)/K;
 }
 
-// k*cot(delta) with Cramer method on the tail:
-// Y[N-2] = A sin(k*X[N-2]) + B cos(k*X[N-2])
-// Y[N-1] = A sin(K*X[N-1]) + B cos(K*X[N-1])
+// return k*cot(delta) with Cramer method on the tail:
+// Y[N-1] = A sin(k*X[N-1]) + B cos(k*X[N-1])
+// Y[N]   = A sin(K*X[N])   + B cos(K*X[N])
 // k*cot(delta) = k*A/B
-double Shift(void)
+double F(void)
 {
-    const double k = sqrt(E/C);
-    const double a = sin(k*X[N-2]);
-    const double b = cos(k*X[N-2]);
-    const double c = sin(k*X[N-1]);
-    const double d = cos(k*X[N-1]);
+    const double k = sqrt(E/K);
+    
+    const double cot = (Y[N-1]*cos(k*X[N])-Y[N]*cos(k*X[N-1])) /
+                       (Y[N]*sin(k*X[N-1])-Y[N-1]*sin(k*X[N]));
 
-    const double det = a*d - b*c;
-    const double A = (Y[N-2]*d - b*Y[N-1])/det;
-    const double B = (a*Y[N-1] - Y[N-2]*c)/det;
-
-    return k*A/B;
+    return k*cot;
 }
 
 int main()
@@ -45,28 +41,20 @@ int main()
     V0 = -60.575; //Mev
     R0 = 1.65; //fm
 
-    X = malloc(N*sizeof(double));
-    Y = malloc(N*sizeof(double));
-    Linspace(X,0.,R,N);
+    X = malloc((N+1)*sizeof(double));
+    Y = malloc((N+1)*sizeof(double));
+    Linspace(X,0.,R,N+1);
     Y[0] = 0.;
     Y[1] = H;
-
-    for (E=0.2; E<10.; E+=0.2) 
+    
+    FILE *outScat; assert(outScat=fopen("scattering.dat","w"));
+    for (double kSq = 0.01; kSq<=1.; kSq += 0.01) 
     {
-        NumerovRight(X,Y,W,N);
-        printf("%.3f\t%.5f\n", E/C , Shift());
+        E = kSq*K;
+        NumerovRight(X,Y,W,N+1);
+        fprintf(outScat, "%.3e\t%.7e\n", kSq, F());
     }
-
-    //puts("*****");
-    //puts("To obtain a scattering length of -23.74,");
-    //puts("with a potential width of 1.65");
-    //printf("The depth needs to be: %.7f\n", V0);
-    //puts("*****");
-
-    //for (int i=0; i<N; i++)
-    //{
-    //    printf("%.16f\n", Y[i]);
-    //}
+    assert( !fclose(outScat) );
 
     free(X);
     free(Y);
